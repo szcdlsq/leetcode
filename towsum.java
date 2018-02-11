a1a9978d9ea77840c9998cebe8172324cabec7c8
szcdlsq <530178318@qq.com>
2018-02-11 14:10:27 +0800

    Update towsum.java

--- towsum.java
+++ towsum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for(int i = 0;i<nums.length;i++){
-            for(int j = 0;j<nums.length;j++){
-                if(nums[i]+nums[j] == target && i != j){
-                   result[0] = i;
-                   result[1] = j;
-                }
-            }
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
         }
-        return result;
+        map.put(nums[i], i);
     }
+    throw new IllegalArgumentException("No two sum solution");
+}
 }
